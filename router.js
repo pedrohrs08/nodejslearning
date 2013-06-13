5a02b24270189ab6940bba8ff56fd4f06c874ea4
pedrohrs08 <pedrohrs08@gmail.com>
2013-06-12 21:49:32 -0300

    added router

--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-function route(pathname) {
+function route(pathname, handle) {
 	console.log("About to route a request for " + pathname);
 	if (typeof handle[pathname] === 'function')
 		handle[pathname]();
